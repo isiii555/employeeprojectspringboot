96b256f4131fd35c901f644eaaf8f05168f1950a
Islam Salamzade (Intern) <Islam.Salamzade@pashabank.az>
2024-07-10 13:40:39 +0400

    test changed

--- src/test/groovy/controller/EmployeeControllerTest.groovy
+++ src/test/groovy/controller/EmployeeControllerTest.groovy
@@ -10,6 +10,7 @@ import com.example.EmployeeProjectApiDemo.helper.ProjectHelper
 import com.example.EmployeeProjectApiDemo.mapper.EmployeeMapper
 import com.example.EmployeeProjectApiDemo.service.EmployeeService
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.mapstruct.factory.Mappers
 import org.skyscreamer.jsonassert.JSONAssert
 import org.springframework.http.HttpStatus
@@ -27,20 +28,13 @@ class EmployeeControllerTest extends Specification{
     private EmployeeService employeeService
     private ObjectMapper objectMapper
     private EmployeeController controller
-    private EmployeeRepository employeeRepository
-    private ProjectRepository projectRepository
-    private ProjectHelper helper
-    private EmployeeMapper mapper
 
     def url = "/api/v1/employees"
 
     def setup() {
-        employeeRepository = Mock()
-        projectRepository = Mock()
         objectMapper = new ObjectMapper()
-        mapper = Mappers.getMapper(EmployeeMapper.class)
-        helper = new ProjectHelper(projectRepository)
-        employeeService = new EmployeeService(employeeRepository,helper,mapper)
+        objectMapper.registerModule(new JavaTimeModule())
+        employeeService = Mock()
         controller = new EmployeeController(employeeService)
         mockMvc = MockMvcBuilders.standaloneSetup(controller)
         .setControllerAdvice(new GlobalExceptionHandler())
@@ -49,20 +43,18 @@ class EmployeeControllerTest extends Specification{
 
     def "should return given employee" () {
         given:
-        def employee = new EmployeeEntity(2,"Kanan","Fatullayev","kananfatullayev@gmail.com","software dev",LocalDate.of(2024,6,21),null)
-
         def employeeDto = new EmployeeDto("Kanan","Fatullayev","kananfatullayev@gmail.com","software dev",LocalDate.of(2024,6,21),null)
 
-        employeeRepository.findById(2) >> employee
-
         when:
-        def result = mockMvc.perform (get("$url/{employeeId}",2)
+        def result = mockMvc.perform (get(url + "/{employeeId}",1)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andReturn().response
 
         then:
-        1 * employeeService.getById(2) >> employeeDto
+//        1 * employeeService.getById(1) >> employeeDto
         result.status == HttpStatus.OK.value()
+        println(result.getContentAsString())
+        println(objectMapper.writeValueAsString(employeeDto))
         JSONAssert.assertEquals(objectMapper.writeValueAsString(employeeDto),result.getContentAsString(),false)
     }
 }
